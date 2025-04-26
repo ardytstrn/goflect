e5e601f18f92cffc51ea63d1009ac9c8fa33f588
ardytstrn <yetistirenarda@gmail.com>
2025-04-26 21:58:44 +0300

    feat: init logger

--- cmd/goflect/main.go
+++ cmd/goflect/main.go
@@ -6,10 +6,15 @@ import (
 	"github.com/ardytstrn/goflect/internal/config"
 	"github.com/ardytstrn/goflect/internal/handlers"
 	"github.com/ardytstrn/goflect/internal/middleware"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
 	cfg := config.Load()
+
+	logger, _ := setupLogger()
+	defer logger.Sync()
 
 	router := http.NewServeMux()
 
@@ -22,5 +27,16 @@ func main() {
 		Handler: middleware.Chain(router, app),
 	}
 
+	logger.Info("Starting server", zap.String("port", cfg.Port))
 	server.ListenAndServe()
+}
+
+func setupLogger() (*zap.Logger, error) {
+	prodConfig := zap.NewProductionConfig()
+	prodConfig.Encoding = "console"
+	prodConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	prodConfig.EncoderConfig.EncodeDuration = zapcore.StringDurationEncoder
+	prodConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	return prodConfig.Build()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ardytstrn/goflect
 
 go 1.24.2
 
-require github.com/bwmarrin/snowflake v0.3.0 // indirect
+require (
+	github.com/bwmarrin/snowflake v0.3.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
